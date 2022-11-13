4fc4a32c55ec714a71220b2e32177d23c379ded3
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2022-11-13 18:32:45 +0530

    changes

--- JS/logic.js
+++ JS/logic.js
@@ -57,6 +57,7 @@ Nav[7].addEventListener("click", function () {
 
 // navbar color logic
 let navbar = document.querySelector(".navbar-container")
+let prevScroll = window.scrollY;
 onscroll = () => {
       // console.log(window.scrollY, navbar.clientHeight)
       if (window.scrollY > 50 || document.documentElement.scrollTop >= 200 ){
@@ -66,9 +67,46 @@ onscroll = () => {
       else{
          navbar.classList.add("navbar-color-transparent");
          navbar.classList.remove("navbar-color");
-         console.log("removed")
+         navbar.classList.remove("navbar-hide");
+         navbar.classList.remove("navbar-show");
       }
+      if (window.scrollY > prevScroll && window.scrollY > 200){
+         navbar.classList.add("navbar-hide");
+         navbar.classList.remove("navbar-show");
+      }
+      else if (window.scrollY < prevScroll && window.scrollY > 200){
+         navbar.classList.remove("navbar-hide");
+         navbar.classList.add("navbar-show");
+      }
+      prevScroll = window.scrollY;
    }
+
+navbar.addEventListener("mouseover", function () {
+   navbar.classList.remove("navbar-hide");
+   navbar.classList.add("navbar-show");
+})
+
+navbar.addEventListener("mouseout", function () {
+   if (window.scrollY > 200){
+      navbar.classList.add("navbar-hide");
+      navbar.classList.remove("navbar-show");
+   }
+   else{
+      navbar.classList.remove("navbar-hide");
+      navbar.classList.remove("navbar-show");
+   }
+})
+
+// const scrollingText = document.querySelector(".marquee>span");
+
+// scrollingText.addEventListener("mouseover", function () {
+//    scrollingText.classList.remove("scrolling");
+//    scrollingText.classList.add(".scrollingslow");
+// })
+// scrollingText.addEventListener("mouseout", function () {
+//    scrollingText.classList.add("scrolling");
+//    scrollingText.classList.remove(".scrollingslow");
+// })
 
 
 
